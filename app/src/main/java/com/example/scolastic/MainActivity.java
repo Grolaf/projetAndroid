9ed6695eded8e22f50360bd0573eb5e6876b6006
MagestiaStudent <fastemail.flyn@gmail.com>
2021-05-19 15:50:22 +0200

    working a moitier : last version

--- app/src/main/java/com/example/scolastic/MainActivity.java
+++ app/src/main/java/com/example/scolastic/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.scolastic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
+
+    // Timeout :
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    // Animation : Bonus will come
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        // Animation : -----
+
+
+        // Splash Screen : Affichage Deuxiéme Page
+
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
