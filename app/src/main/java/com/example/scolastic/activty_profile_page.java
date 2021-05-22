a382292224bd604720d5bd477135fc7b4f849e90
MagestiaStudent <fastemail.flyn@gmail.com>
2021-05-22 03:13:03 +0200

    soo

---
+++ app/src/main/java/com/example/scolastic/activty_profile_page.java
@@ -0,0 +1,14 @@
+package com.example.scolastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activty_profile_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activty_profile_page);
+    }
+}
