ab650945367b413a83bddc46bf64936484473c2f
MagestiaStudent <fastemail.flyn@gmail.com>
2021-05-29 22:20:53 +0200

    Espace Eleve ; Pas de Bras pas de chocolat

---
+++ app/src/main/java/com/example/scolastic/EleveAdapterViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.scolastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EleveAdapterViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eleve_adapter_view);
+    }
+}
