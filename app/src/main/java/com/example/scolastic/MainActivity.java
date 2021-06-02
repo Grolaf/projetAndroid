1b768cf372d013b65654d348fa81f1fda9119b88
MagestiaStudent <fastemail.flyn@gmail.com>
2021-06-03 00:06:19 +0200

    Résumé changement :
     - UX changé
     - Couleur & string harmonisé
     - Nouveau splash screen avec bouton
     - Page Inscription plus simple
     - Page Matiére/Principale :
        - Ajout d'un titre et d'une consigne
        - Ajout d'un icon menu servira information
        - card view en jaune a faire fonctionner
     - Page Felication et erreur harmonisé
     - Page des niveau réalisé
    
     A venir --> à crée en Avance : Page QCM
     INFO : drawble sur discord

--- app/src/main/java/com/example/scolastic/MainActivity.java
+++ app/src/main/java/com/example/scolastic/MainActivity.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import tests.ExercicesTests;
 
 public class  MainActivity extends AppCompatActivity {
 
-    // Timeout :
+
 
     private static int SPLASH_TIME_OUT = 3000;
-
-    // Animation : Bonus will come
-
+    Button start_btn;
 
 
     @Override
@@ -28,17 +28,25 @@ public class  MainActivity extends AppCompatActivity {
 
         ExercicesTests tests = new ExercicesTests();
 
-        // Animation : -----
+        start_btn = findViewById(R.id.start_btn);
 
-        // Splash Screen : Affichage Deuxiéme Page
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), InscriptionActivity.class));
+            }
+        });
+
+       /* new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, InscriptionActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },SPLASH_TIME_OUT);
+        },SPLASH_TIME_OUT); */
     }
 }
